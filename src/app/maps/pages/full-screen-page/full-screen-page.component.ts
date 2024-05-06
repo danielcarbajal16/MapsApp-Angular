2b8867b3f3d2e554c1cd236b50f6eb84d6e325db
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2024-05-06 15:50:00 -0600

    New script and changes on FullPageComponent

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as mapboxgl from "mapbox-gl";
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiZGFuaWNhcmJhamFsIiwiYSI6ImNsdWtzaHUxMDBwazQybW1paHN1emgyMngifQ.HyYWkypBkZbmBCEPO949eg';
 
 @Component({
   selector: 'maps-full-screen-page',
@@ -6,6 +10,14 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.5, 40],
+      zoom: 9
+    })
+  }
 
 }
